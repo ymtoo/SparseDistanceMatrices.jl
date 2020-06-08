3c390f7ba685ca55e4b0104104bbfd4c51ed699a
ymtoo <ymtoo86@gmail.com>
2020-06-08 18:54:52 +0800

    test(runtests): more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,8 +15,23 @@ using Test
         D[40,2] = b
 
         @test size(D) == (100, 100)
-        @test D[1,1] == typemax(T)
+        [@test D[i,i] == typemax(T) for i in 1:N]
         @test D[1,100] == typemax(T)
+        @test D[1,20] == a
+        @test D[40,2] == b
+        @test D != D'
+        @test symmetrize!(D) == D'
+
+        ndval = T <: Integer ? trunc(T, 1000.0) : T(1000.0)
+        D = SparseDistanceMatrix(N, Int[], Int[], T[], ndval)
+        a = T <: Integer ? trunc(T, 10.0) : T(10.0)
+        b = T <: Integer ? trunc(T, 12.3) : T(12.3)
+        D[1,20] = a
+        D[40,2] = b
+
+        @test size(D) == (100, 100)
+        [@test D[i,i] == ndval for i in 1:N]
+        @test D[1,100] == ndval
         @test D[1,20] == a
         @test D[40,2] == b
         @test D != D'
